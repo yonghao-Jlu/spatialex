e847c144df54845969d30399292a3906440e5191
Sherlock <129871227+Chao1112@users.noreply.github.com>
2025-11-30 21:15:10 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,11 +64,6 @@ autodoc_mock_imports = [
 # ---- HTML theme ----
 html_theme = "sphinx_rtd_theme"
 
-html_theme_options = {
-    # ...existing options...
-    "navigation_depth": 1,
-}
-
 # ---- Source suffix ----
 source_suffix = ".rst"
 templates_path = ["_templates"]
